edbdefbeed73286ab35e1586d8d0616214d65ede
klnk <klnkklnk@icloud.com>
2024-04-05 17:54:42 +0200

    small changes of routes file: combination of 'get' and 'post' routes for '/login/' changed on 'any' route

--- up.schedule/install/routes/schedule.php
+++ up.schedule/install/routes/schedule.php
@@ -15,8 +15,8 @@ return static function(RoutingConfigurator $routes) {
 	$routes->get('/statistics/', new PublicPageController('/local/modules/up.schedule/views/statistics.php'));
 	$routes->get('/add-couple/', new PublicPageController('/local/modules/up.schedule/views/add-couple.php'));
 
-	$routes->get('/login/', new PublicPageController('/local/modules/up.schedule/views/login.php'));
-	$routes->post('/login/', new PublicPageController('/local/modules/up.schedule/views/login.php'));
+
+	$routes->any('/login/', new PublicPageController('/local/modules/up.schedule/views/login.php'));
 	$routes->get('/logout/', function () {
 		global $USER;
 		$USER->Logout();
