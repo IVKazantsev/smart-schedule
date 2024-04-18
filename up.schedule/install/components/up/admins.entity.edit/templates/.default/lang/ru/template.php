17d0a6e48f7cef151aa3954aa04c4fc4a7a61a31
Ilya Kazancev <ilyakazancev24112003@gmail.com>
2024-04-18 14:37:19 +0200

    visual of edit form

---
+++ up.schedule/install/components/up/admins.entity.edit/templates/.default/lang/ru/template.php
@@ -0,0 +1,13 @@
+<?php
+
+$MESS['ID'] = 'ID';
+$MESS['NAME'] = 'Имя';
+$MESS['LAST_NAME'] = 'Фамилия';
+$MESS['EMAIL'] = 'Почта';
+$MESS['ROLE'] = 'Роль';
+$MESS['GROUP'] = 'Группа';
+$MESS['TITLE'] = 'Название';
+$MESS['NUMBER'] = 'Номер';
+$MESS['TYPE'] = 'Тип';
+
+$MESS['ENTITY_EDIT'] = 'Редактирование';
